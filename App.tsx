2d2959ae11835fa4d22bd3099e4a71e6546cddc4
FerLagHer <flagunahermida@gmail.com>
2023-01-07 02:14:29 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Image, StyleSheet, Text, View } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 
 export default function App() {
+  const [isShow, setIsShow] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>¡Hola, Coder!</Text>
+      <Button
+        title="Sorpresa 🐈"
+        color={"#b60c18"}
+        onPress={() => setIsShow(wasShow => !wasShow)}
+      />
+      <Image
+        source={{ uri: "http://placekitten.com/200/300" }}
+        style={[styles.image, isShow ? styles.show : styles.hide]}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#232325",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  image: {
+    width: 200,
+    height: 300,
+    borderRadius: 10,
+    marginVertical: 20,
+  },
+  show: {
+    display: "flex",
+  },
+  hide: {
+    display: "none",
   },
 });
